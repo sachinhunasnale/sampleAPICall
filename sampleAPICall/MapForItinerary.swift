90f1a6328e0ae4ee189f007e8c17eef3a3ae34c6
sachinhunasnale@gmail.com <sachinhunasnale@gmail.com>
2020-02-17 18:54:22 +0530

    first commit

---
+++ sampleAPICall/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  sampleAPICall
+//
+//  Created by Muppoori Bharathi on 17/02/20.
+//  Copyright © 2020 3i Infotech. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary:NSObject {
+    
+}
