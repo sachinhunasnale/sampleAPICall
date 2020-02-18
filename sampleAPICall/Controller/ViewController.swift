f6883209e2d063e603a4437504b6156a6ed1479e
sachinhunasnale@gmail.com <sachinhunasnale@gmail.com>
2020-02-18 19:13:37 +0530

    change VC file.

--- sampleAPICall/Controller/ViewController.swift
+++ sampleAPICall/Controller/ViewController.swift
@@ -58,7 +58,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBAction func buttonAction(_ sender: Any) {
         print("button pressed");
-        print("finished");
+        print("finished push on master branch");
     }
 }
 
